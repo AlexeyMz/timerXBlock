6f499e6ca39556aa0f395e950454b51c0c1bd01f
Alexey Morozov <alexey.morozov@live.ru>
2015-09-02 16:29:39 +0300

    Fix edX js fail

--- timer/static/js/jquery.timeTo.js
+++ timer/static/js/jquery.timeTo.js
@@ -10,17 +10,8 @@
 'use strict';
 
 (function (factory) {
-    if (typeof exports === 'object') {
-        // CommonJS (Node)
-        var jQuery = require('jquery');
-        module.exports = factory(jQuery || $);
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD
-        define(['jquery'], factory);
-    } else {
-        // globals
-        factory(jQuery || $);
-    }
+    // globals
+    factory(jQuery || $);
 }(function ($) {
 
     var defaults = {
