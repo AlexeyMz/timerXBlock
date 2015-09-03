fe6454c75921903bb8e8175d7aaef885022a9a1a
Alexey Morozov <alexey.morozov@live.ru>
2015-09-03 15:45:06 +0300

    Added modal overlay

--- timer/static/js/timer_view.js
+++ timer/static/js/timer_view.js
@@ -9,28 +9,47 @@ function timerXBlockInitView(runtime, element) {
     var $countdonwn = element.find('.countdown');
     var usageId = element.attr('data-usage-id');
     var key = "timerXBlock_" + encodeURIComponent(usageId);
-    var startDate = localStorage.getItem(key);
-    if (startDate) {
-        startDate = new Date(startDate);
+    var startDateText = localStorage.getItem(key);
+    if (startText) {
+        resumeTimer(new Date(startDateText));
     } else {
-        startDate = new Date();
-        localStorage.setItem(key, startDate);
-    }
-    var limitSeconds = parseInt($countdonwn.attr('data-seconds'), 10);
-    var now = new Date();
-    var secondsLeft = limitSeconds - (now - startDate) / 1000;
-    console.log('seconds left: ' + secondsLeft);
-    if (secondsLeft > 0) {
-        $countdonwn.timeTo({
-            seconds: limitSeconds,
-            displayHours: false,
-            callback:  onLimitReached
+        showModalOverlay("START_EXAM", "ACTION_BEGIN", function () {
+            startDate = new Date();
+            localStorage.setItem(key, startDate);
+            resumeTimer(startDate);
         });
-    } else {
-        $countdonwn.timeTo({ seconds: 1, displayHours: false });
-        onLimitReached();
+    }
+    
+    function resumeTimer(startDate) {
+        var limitSeconds = parseInt($countdonwn.attr('data-seconds'), 10);
+        var now = new Date();
+        var secondsLeft = limitSeconds - (now - startDate) / 1000;
+        console.log('seconds left: ' + secondsLeft);
+        if (secondsLeft > 0) {
+            $countdonwn.timeTo({
+                seconds: limitSeconds,
+                displayHours: false,
+                callback:  onLimitReached
+            });
+        } else {
+            $countdonwn.timeTo({ seconds: 1, displayHours: false, start: false });
+            onLimitReached();
+        }
     }
     function onLimitReached() {
-        alert('end!');
+        showModalOverlay("TIME_OVER", "ACTION_SEE_RESULTS", function () {
+            window.location.href = "../../progress";
+        });
+    }
+    function showModalOverlay(contentText, actionText, actionCallback) {
+        var $overlay = $(".timerModalOverlay");
+        if ($overlay.length === 0) {
+            $overlay = $("<div class='timerModalOverlay'/>").appendTo(document.body);
+        }
+        $overlay.empty()
+            .append($("<div class='timerModalContent'/>").text(contentText))
+            .append($("<a class='timerModalAction'>")
+                .text(actionText)
+                .click(actionCallback));
     }
 }
