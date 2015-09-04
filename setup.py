d57da863a0f3d90c81add042567cc91aa13d57e7
Alexey Morozov <alexey.morozov@live.ru>
2015-09-04 15:38:11 +0300

    Implemented all modal dialogs and ajax requests

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def package_data(pkg, root):
 
 setup(
     name='timer-xblock',
-    version='0.2.38',
+    version='0.2.39',
     description='This XBlock provides a way to setup a time limit of exam course.',
     packages=[
         'timer',
